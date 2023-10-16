5b78e55d2bbfe8ceae2cdc6aca6d45bd7b9f0aff
ZeGuilhermeYung <jgryung@gmail.com>
2023-10-15 21:11:27 -0300

    hide photoRegex in schemas

--- src/schemas/users.schemas.js
+++ src/schemas/users.schemas.js
@@ -1,13 +1,13 @@
 import joi from 'joi';
 
 const emailRegex = /^[\w-]+(\.[\w-]+)*@[\w-]+(\.[\w-]+)*(\.[a-z]{2,})$/;
-const photoRegex = /(http[s]?:\/\/.*\.(?:png|jpg|gif|svg|jpeg))/i;
+//const photoRegex = /(http[s]?:\/\/.*\.(?:png|jpg|gif|svg|jpeg))/i;
 
 const userSchema = joi.object({
     username: joi.string().required(),
     email: joi.string().pattern(emailRegex).required(),
     password: joi.string().required(),
-    photo: joi.string().pattern(photoRegex).empty('')
+    photo: joi.string().empty('')
 });
 
 export default userSchema;
